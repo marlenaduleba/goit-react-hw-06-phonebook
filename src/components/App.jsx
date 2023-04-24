fa406562c5501843b7b66f75e8866df917a8987e
Marlena Dulęba <marlena_duleba@o2.pl>
2023-04-24 18:49:44 +0200

    Revert "Revert "state structure changed""
    This reverts commit 363b32fe57000915324966d579680d6c81277437.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,17 @@
 import Container from "./Layout/Layout";
 import { Phonebook } from "./Phonebook/Phonebook";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "redux/store";
 
 export const App = () => {
   return (
-    <Container>
-      <Phonebook />
-    </Container>
+    <Provider store={store} >
+      <PersistGate loading={null} persistor={persistor} >
+        <Container>
+          <Phonebook />
+        </Container>
+      </PersistGate>
+    </Provider>
   );
 };
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,14 +1,24 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
+import persistReducer from 'redux-persist/es/persistReducer';
+import storage from 'redux-persist/lib/storage';
 
-const contactsInitialState = [];
+const defaultContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
 const contactsSlice = createSlice({
   name: 'contacts',
-  initialState: contactsInitialState,
+  initialState: {
+    items: defaultContacts,
+    filter: '',
+  },
   reducers: {
     addContact: {
       reducer(state, action) {
-        state.push(action.payload);
+        state.items.push(action.payload);
       },
       prepare(name, number) {
         return {
@@ -21,14 +31,25 @@ const contactsSlice = createSlice({
       },
     },
     deleteContact(state, action) {
-      const contact = state.findIndex(contact => contact.id === action.payload);
-      state.splice(contact, 1);
+      const contact = state.items.findIndex(contact => contact.id === action.payload);
+      state.items.splice(contact, 1);
     },
     setContacts(state, action) {
-      state = action.payload;
+      state.items = action.payload;
     },
+    setFilter(state, action) {
+      state.filter = action.payload;
+    }
   },
 });
 
-export const { addContact, deleteContact, setContacts } = contactsSlice.actions;
-export const contactsReducer = contactsSlice.reducer;
+const persistConfig = {
+  key: 'contacts',
+  storage,
+  whitelist: ['items', 'filter'],
+}
+
+
+export const contactsReducer = persistReducer(persistConfig, contactsSlice.reducer);
+
+export const { addContact, deleteContact, setContacts, setFilter } = contactsSlice.actions;
