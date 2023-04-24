1775bdc407c1065514ada19552497bebfd5ad651
Marlena Dulęba <marlena_duleba@o2.pl>
2023-04-24 19:58:08 +0200

    Phonebook  fixed
    This reverts commit b791533578c02ef83a20ae4ed1c0200aff6f5dba.

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -2,22 +2,13 @@ import { Section } from 'components/Section/Section';
 import { Filter } from 'components/Filter/Filter';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { ContactList } from 'components/ContactList/ContactList';
+import { useSelector } from 'react-redux';
+import { getContacts } from 'redux/selectors';
 
 import css from './Phonebook.module.css';
 
 export const Phonebook = () => {
-  // const [contacts, setContacts] = useState([]);
-  // const [filter, setFilter] = useState('');
-
-  // const handleFilter = e => {
-  //   setFilter(e.target.value);
-  // };
-
-  // const renderContacts = useMemo(() => {
-  //   return contacts.filter(({ name }) =>
-  //     name.toLowerCase().includes(filter.toLowerCase().trim())
-  //   );
-  // }, [filter, contacts]);
+  const contacts = useSelector(getContacts);
 
   return (
     <div className={css.box}>
@@ -26,14 +17,14 @@ export const Phonebook = () => {
       </Section>
 
       <Section title="Contacts">
-        {/* {contacts.length !== 0 ? ( */}
+        {contacts.length > 0 ? (
           <Filter />
-        {/* ) : (
+        ) : (
           <p className={css.message}>
             Looks like you don`t have any contacts yet or just clear them all.
             Please add new contact.
           </p>
-        )} */}
+        )}
 
         <ContactList />
       </Section>
