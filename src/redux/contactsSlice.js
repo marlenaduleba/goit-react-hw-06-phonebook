d04bbb362af48a9500f0d7fe8d79e832cb73ed11
Marlena Dulęba <marlena_duleba@o2.pl>
2023-04-25 07:14:16 +0200

    defaultContacts removed

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -2,17 +2,11 @@ import { createSlice, nanoid } from '@reduxjs/toolkit';
 import persistReducer from 'redux-persist/es/persistReducer';
 import storage from 'redux-persist/lib/storage';
 
-const defaultContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
 
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: {
-    items: defaultContacts,
+    items: [],
     filter: '',
   },
   reducers: {
