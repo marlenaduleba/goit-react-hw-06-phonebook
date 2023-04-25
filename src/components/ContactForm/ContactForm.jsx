65d2031000dda27abd437329325c7def7718d5e4
Marlena Dulęba <marlena_duleba@o2.pl>
2023-04-25 09:12:36 +0200

    regex pattern corrected

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -63,7 +63,7 @@ export const ContactForm = () => {
         <input
           type="text"
           name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          pattern="^[a-zA-Z\\u0080-\\uFFFF]+(([\\' -][a-zA-Z\\u0080-\\uFFFF ])?[a-zA-Z\\u0080-\\uFFFF]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           value={name}
           onChange={handleInputChange}
